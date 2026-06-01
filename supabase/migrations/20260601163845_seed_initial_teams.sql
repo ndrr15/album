3b22499f342ee0ddb730cadd5825d03634ffc965
ndrr15 <ndrr15@gmail.com>
2026-06-01 11:41:59 -0500

    feat(database): add initial seed data for world cup teams

---
+++ supabase/migrations/20260601163845_seed_initial_teams.sql
@@ -0,0 +1,78 @@
+insert into public.teams (
+    code,
+    name,
+    group_name,
+    ranking
+)
+values
+-- Grupo A
+('MEX', 'Mexico', 'A', 17),
+('RSA', 'South Africa', 'A', 56),
+('KOR', 'Korea Republic', 'A', 23),
+('CZE', 'Czechia', 'A', 42),
+
+-- Grupo B
+('CAN', 'Canada', 'B', 31),
+('BIH', 'Bosnia and Herzegovina', 'B', 74),
+('QAT', 'Qatar', 'B', 48),
+('SUI', 'Switzerland', 'B', 20),
+
+-- Grupo C
+('BRA', 'Brazil', 'C', 5),
+('MAR', 'Morocco', 'C', 12),
+('HAI', 'Haiti', 'C', 83),
+('SCO', 'Scotland', 'C', 39),
+
+-- Grupo D
+('USA', 'United States', 'D', 16),
+('PAR', 'Paraguay', 'D', 53),
+('AUS', 'Australia', 'D', 24),
+('TUR', 'Türkiye', 'D', 28),
+
+-- Grupo E
+('GER', 'Germany', 'E', 10),
+('CUW', 'Curacao', 'E', 90),
+('CIV', 'Cote d''Ivoire', 'E', 41),
+('ECU', 'Ecuador', 'E', 30),
+
+-- Grupo F
+('NED', 'Netherlands', 'F', 8),
+('JPN', 'Japan', 'F', 15),
+('SWE', 'Sweden', 'F', 27),
+('TUN', 'Tunisia', 'F', 49),
+
+-- Grupo G
+('BEL', 'Belgium', 'G', 6),
+('EGY', 'Egypt', 'G', 36),
+('IRN', 'Iran', 'G', 21),
+('NZL', 'New Zealand', 'G', 94),
+
+-- Grupo H
+('ESP', 'Spain', 'H', 3),
+('CPV', 'Cape Verde', 'H', 65),
+('KSA', 'Saudi Arabia', 'H', 58),
+('URU', 'Uruguay', 'H', 11),
+
+-- Grupo I
+('FRA', 'France', 'I', 2),
+('SEN', 'Senegal', 'I', 18),
+('IRQ', 'Iraq', 'I', 59),
+('NOR', 'Norway', 'I', 43),
+
+-- Grupo J
+('ARG', 'Argentina', 'J', 1),
+('ALG', 'Algeria', 'J', 37),
+('AUT', 'Austria', 'J', 22),
+('JOR', 'Jordan', 'J', 62),
+
+-- Grupo K
+('POR', 'Portugal', 'K', 7),
+('COD', 'DR Congo', 'K', 61),
+('UZB', 'Uzbekistan', 'K', 57),
+('COL', 'Colombia', 'K', 14),
+
+-- Grupo L
+('ENG', 'England', 'L', 4),
+('CRO', 'Croatia', 'L', 13),
+('GHA', 'Ghana', 'L', 64),
+('PAN', 'Panama', 'L', 45);
