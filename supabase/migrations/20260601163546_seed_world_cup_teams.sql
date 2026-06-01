327295eb3a60e80efac60254a832318f27d86947
ndrr15 <ndrr15@gmail.com>
2026-06-01 11:36:34 -0500

    feat(database): seed world cup teams into groups table

---
+++ supabase/migrations/20260601163546_seed_world_cup_teams.sql
@@ -0,0 +1,14 @@
+insert into public.groups(code, name)
+values
+('A','Group A'),
+('B','Group B'),
+('C','Group C'),
+('D','Group D'),
+('E','Group E'),
+('F','Group F'),
+('G','Group G'),
+('H','Group H'),
+('I','Group I'),
+('J','Group J'),
+('K','Group K'),
+('L','Group L');
